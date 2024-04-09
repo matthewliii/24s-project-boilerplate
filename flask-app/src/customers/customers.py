4331d0301f9cc83b46ff8013b04a9519e64cd0f4
matthewliii <matthewli4859@gmail.com>
2024-04-08 21:18:58 -0400

    Homework6

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -2,7 +2,7 @@
 # Sample customers blueprint of endpoints
 # Remove this file if you are not using it in your project
 ########################################################
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -13,7 +13,7 @@ customers = Blueprint('customers', __name__)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
+    cursor.execute('select id, company, last_name,\
         first_name, job_title, business_phone from customers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -39,3 +39,34 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+@customers.route('/customers', methods=['PUT'])
+def update_customer_info():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    id = the_data['id']
+    company = the_data['company']
+    last_name = the_data['last_name']
+    first_name = the_data['first_name']
+    job_title = the_data['job_title']
+    business_phone = the_data['business_phone']
+
+    # Constructing the query
+    query = 'update customers set company = "'
+    query += company  + '", last_name = "'
+    query += last_name + '", first_name = "'
+    query += first_name + '", job_title = "' 
+    query += job_title + '", business_phone = "'
+    query += business_phone + '" where id = "' + str(id) + '";'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
