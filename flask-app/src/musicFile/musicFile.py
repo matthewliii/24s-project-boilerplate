46d2edaf1854d0033a7f594c38e0385531927b4a
chancod21 <chan.cod@northeastern.edu>
2024-04-18 19:10:07 -0400

    Get route for all music files in database

--- flask-app/src/musicFile/musicFile.py
+++ flask-app/src/musicFile/musicFile.py
@@ -101,6 +101,21 @@ def get_musicFiles_Key(Key):
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get all music files from the DB
+@musicFile.route('/all_musicFiles', methods=['GET'])
+def get_all_musicFiles():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from musicFile')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
 # Update generic info of music file uploaded by a specific user
 @musicFile.route('/musicFile/<userID>/<MusicFileID>', methods=['PUT'])
 def update_musicFile_info():
